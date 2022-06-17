a4df60370dde25d475a2696840a4d0c47d133057
Raj-Patel97 <raj.patel@credera.com>
2022-06-17 12:35:23 -0500

    adding hide n seek link

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
 import "./App.css";
 import { Link } from "react-router-dom";
 import { Outlet } from "react-router-dom";
+import { useState } from "react";
 
 function App() {
+  const [style,setStyle] = useState({})
+
+  const moveLink = () =>{
+    const randX = Math.floor(Math.random() * (window.innerWidth - 100));
+    const randY = Math.floor(Math.random() * (window.innerHeight - 100));
+    const delay = Math.floor(Math.random() * 200);
+    setStyle({transitionDelay: `${delay}ms`,position: "absolute",left: `${randX}px`,top: `${randY}px`, fontSize: "40px" })
+  }
+
   return (
     <div className="app">
       <nav className="app-nav">
         <Link to="/">Home</Link>
         <Link to="objective">Objective</Link>
-        <Link to="teams">Teams</Link>
+        <Link style={style} onClick={() =>setStyle({})} onMouseEnter={moveLink} className="hide-n-seek-btn" to="teams">Teams</Link>
         <Link to="thanks">Thank You</Link>
       </nav>
       <div className="app-content">
